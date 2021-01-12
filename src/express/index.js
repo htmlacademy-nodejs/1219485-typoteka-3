dff5b6003eefd7903761d431116ff5cdcb9dd5d0
Vyacheslav <vyachesl.n@gmail.com>
2021-01-12 17:55:00 +0600

    Личный M3-T2-A1

--- src/express/index.js
+++ src/express/index.js
@@ -1,17 +1,34 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const articlesRoutes = require(`./routes/articles-routes`);
 
+const {HttpCode} = require(`../service/cli/const`);
+
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
+
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND).render(`404`);
+});
+
+app.use((err, req, res, _next) => {
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`500`);
+});
 
 app.listen(DEFAULT_PORT);
